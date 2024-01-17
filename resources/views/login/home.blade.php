19b9e0445bdaa664cad0f3797839393c6de75f09
rukaachan <novops911@gmail.com>
2024-01-17 20:16:05 +0700

    update:finishing home view

--- resources/views/login/home.blade.php
+++ resources/views/login/home.blade.php
@@ -11,7 +11,7 @@
 
 <body>
 
-    <main class="h-screen flex flex-wrap overflow-hidden">
+    <main class="h-screen flex flex-wrap overflow-hidden relative">
         <img src="{{ asset('assets/vector_left.png') }}"
             class="absolute left-0 top-1/2 transform -translate-y-1/2 z-20 h-full" />
 
@@ -19,43 +19,45 @@
 
             <div class="bg-opacity-80 absolute top-0 left-0 w-full h-full z-10 bg-red-900"></div>
 
+            <div class="absolute inset-0 flex justify-center items-center">
+                <img src="{{ asset('assets/smk1kotabekasi.png') }}" class="w-[65%] z-30">
+            </div>
+
             <img src="{{ asset('assets/movie.png') }}" class="bg-cover w-full h-full object-cover" />
 
             <img src="{{ asset('assets/lotre_kiri.png') }}"
-                class="absolute left-0 top-1/2 transform -translate-y-1/2 w-full h-full z-10" />
+                class="absolute top-1/2 transform -translate-y-1/2 -translate-x-[15%] h-full z-10" />
+
+            <img src="{{ asset('assets/clapperboard.png') }}"
+                class="absolute top-1/2 transform -translate-y-1/2 -translate-x-[15%] h-1/4 z-20" />
+
+            <img src="{{ asset('assets/lampu_kanan.png') }}"
+                class="absolute left-0 top-[10%] transform -translate-y-1/2 z-20 h-1/4" />
+
+            <img src="{{ asset('assets/persegiHitam.png') }}"
+                class="absolute left-[4%] top-[10%] transform -translate-y-1/2 z-10 h-[10%] w-1/6" />
 
             <img src="{{ asset('assets/chair.png') }}" class="w-full absolute bottom-0 left-0 z-10" />
 
             <img src="{{ asset('assets/lotre_kanan.png') }}"
-                class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 w-full h-full" />
+                class="absolute right-0 top-1/2 transform -translate-y-1/2 translate-x-[15%] z-10 h-full" />
+
+            <img src="{{ asset('assets/lampu_kiri.png') }}"
+                class="absolute right-0 top-[10%] transform -translate-y-1/2 z-20 h-1/4" />
+
+            <img src="{{ asset('assets/hapy.png') }}"
+                class="absolute right-[5%] top-1/2 transform -translate-y-1/2 z-30 h-1/4" />
+
+            <img src="{{ asset('assets/sad.png') }}"
+                class="absolute right-0 top-1/2 transform -translate-y-1/2 z-20 h-1/4" />
         </div>
 
         <img src="{{ asset('assets/vector_right.png') }}"
             class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 h-full" />
+
     </main>
 
-    {{-- <main class="min-h-screen flex flex-wrap min-w-full">
 
-        <div class="bg-opacity-80 absolute top-0 left-0 w-full h-full z-10 bg-red-900"></div>
-
-        <img src="{{ asset('assets/vector_left.png') }}"
-            class="absolute left-0 top-1/2 transform -translate-y-1/2 z-20" />
-
-        <div class="relative w-full">
-            <img src="{{ asset('assets/movie.png') }}" class="bg-cover w-full h-full object-cover" />
-
-            <img src="{{ asset('assets/lotre_kiri.png') }}"
-                class="absolute left-0 top-1/2 transform -translate-y-1/2 w-full z-10" />
-
-            <img src="{{ asset('assets/chair.png') }}" class="w-full absolute bottom-0 left-0 z-10" />
-
-            <img src="{{ asset('assets/lotre_kanan.png') }}"
-                class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 w-full" />
-        </div>
-
-        <img src="{{ asset('assets/vector_right.png') }}"
-            class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10" />
-    </main> --}}
 </body>
 
 </html>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -48,6 +48,24 @@
                         placeholder="Name">
                 </div>
 
+                <div class="flex absolute bottom-4 left-1/2 w-96 h-12">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                        <svg width="22" height="24" viewBox="0 0 22 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <g id="Iconly/Bold/Profile">
+                                <g id="Profile">
+                                    <path id="Profile_2" fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M15.3021 7.34254C15.3021 10.3018 13.217 12.7007 10.6448 12.7007C8.07271 12.7007 5.98759 10.3018 5.98759 7.34254C5.98759 4.38331 8.07271 1.98438 10.6448 1.98438C13.217 1.98438 15.3021 4.38331 15.3021 7.34254ZM14.6423 15.7196C16.1231 16.0551 17.0898 16.6024 17.5041 17.4851C17.8153 18.174 17.8153 18.9942 17.5041 19.6831C17.0898 20.5658 16.1614 21.1484 14.6269 21.4486C13.9431 21.6028 13.251 21.7031 12.5553 21.7487C11.9108 21.8281 11.2663 21.8281 10.6142 21.8281H9.44025C9.19473 21.7928 8.95688 21.7752 8.7267 21.7752C8.031 21.7351 7.3387 21.6378 6.6551 21.4839C5.17429 21.1661 4.20755 20.6011 3.79323 19.7184C3.63329 19.3765 3.54896 18.9945 3.54771 18.6062C3.54437 18.2155 3.62614 17.8301 3.78556 17.4851C4.1922 16.6024 5.15895 16.0286 6.6551 15.7196C7.34171 15.5689 8.03638 15.4715 8.73437 15.4283C10.0112 15.3132 11.2939 15.3132 12.5707 15.4283C13.266 15.4737 13.9579 15.571 14.6423 15.7196Z"
+                                        fill="#868AA5" />
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <input type="text" id="email-address-icon"
+                        class="bg-gray-50 border border-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 pl-12 placeholder:text-gray-400"
+                        placeholder="Name">
+                </div>
+
             </div>
         </form>
 
